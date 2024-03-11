21233b4c4af58a65da7b1ce260e9ae00891cad4b
strikef <seunghyeon.nam@sf.snu.ac.kr>
2024-03-11 15:24:58 +0900

    update llvm build script for linux

--- practice-materials/scripts-linux/install-llvm.sh
+++ practice-materials/scripts-linux/install-llvm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 LLVM_VER="18.1.0"
+# Edit this directory
+LLVM_DIR=~/llvm-$LLVM_VER
+# ARM users should use AArch64 instead of X86
+ARCH=X86
 
 echo "[SCRIPT] Changing APT mirror for faster download..."
 sudo apt update
@@ -9,31 +13,49 @@ sudo sed -i 's|http://archive.ubuntu.com|https://ftp.kaist.ac.kr|g' /etc/apt/sou
 
 echo "[SCRIPT] Installing dependencies..."
 sudo apt update
-sudo apt -y install git g++ cmake ninja-build python3-distutils zlib1g-dev \
-libncurses-dev
+sudo apt -y install git g++ cmake ninja-build python3-distutils zlib1g-dev libncurses-dev
 
 echo "[SCRIPT] Cloning LLVM-$LLVM_VER source..."
 LLVM_SRC_DIR=llvm-project-$LLVM_VER
 git clone -b llvmorg-$LLVM_VER https://github.com/llvm/llvm-project.git --depth 1 $LLVM_SRC_DIR
 cd $LLVM_SRC_DIR
 
-echo "[SCRIPT] Creating installation directory..."
-LLVM_DIR=~/llvm-$LLVM_VER # Edit this directory
-mkdir $LLVM_DIR
+echo "[SCRIPT] Building and installing libc++..."
+sleep 2
+BSTRP_BUILD_DIR=build-$LLVM_VER-tmp
+cmake -G Ninja -S llvm -B $BSTRP_BUILD_DIR \
+    -DLLVM_ENABLE_PROJECTS="clang;lld" \
+    -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
+    -DLLVM_TARGETS_TO_BUILD="$ARCH" \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DLLVM_RAM_PER_COMPILE_JOB=256 \
+    -DLLVM_RAM_PER_LINK_JOB=1536 \
+    -DLLVM_USE_LINKER=gold \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$LLVM_DIR
+cmake --build $BSTRP_BUILD_DIR
+cmake --install $BSTRP_BUILD_DIR
 
 echo "[SCRIPT] Building and installing LLVM..."
+sleep 2
 BUILD_DIR=build-$LLVM_VER
+CLANG=$LLVM_DIR/bin/clang
+CLANGXX=$LLVM_DIR/bin/clang++
+LLD=$LLVM_DIR/bin/ld.lld
 cmake -G Ninja -S llvm -B $BUILD_DIR \
+    -DCMAKE_C_COMPILER=$CLANG \
+    -DCMAKE_CXX_COMPILER=$CLANGXX \
     -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lldb;lld" \
-    -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
     -DLLVM_INSTALL_UTILS=ON \
-    -DLLVM_TARGETS_TO_BUILD="X86" \
+    -DLLVM_TARGETS_TO_BUILD="$ARCH" \
     -DLLVM_ENABLE_RTTI=ON \
     -DLLVM_ENABLE_EH=ON \
     -DLLVM_ENABLE_ASSERTIONS=ON \
     -DLLVM_RAM_PER_COMPILE_JOB=256 \
     -DLLVM_RAM_PER_LINK_JOB=1536 \
-    -DLLVM_USE_LINKER=gold \
+    -DLLVM_ENABLE_LIBCXX=ON \
+    -DLLVM_USE_LINKER=$LLD \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$LLVM_DIR
